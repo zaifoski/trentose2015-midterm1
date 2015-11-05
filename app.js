821458981fec5592de1bc4ac6987b771b52645a1
Marcos Baez <mbaezpy@gmail.com>
2015-11-05 08:39:40 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/* your code should go in this file */
+
+/* 
+ * The data is available in the array *data*
+ * Each element of the array has the following structure:
+ *  {
+ *    phrase_en : "I'm a man",        // sentence in english
+ *    phrase_de : "Ich bin ein Mann"  // sentence in german
+ *  }
+ */ 
+
+var tmpl = ' <li id="ID">' +
+           '  <h3>SENTENCE</h3>' +
+           ' </li> ';
+
+$(document).ready(function(){
+
+  
+});
+
+
+
+
+
+
+
---
+++ data.js
@@ -0,0 +1,32 @@
+/* Don't modify this file */
+
+var data = [
+  {
+    phrase_en : "I'm a man",
+    phrase_de : "Ich bin ein Mann"
+  },
+  {
+    phrase_en : "I'm a woman",
+    phrase_de : "Ich bin eine Frau"
+  },
+  {
+    phrase_en : "I eat an apple",
+    phrase_de : "Ich esse einen Apfel"
+  },
+  {
+    phrase_en : "I have a dog",
+    phrase_de : "Ich habe einen Hund"
+  },
+  {
+    phrase_en : "I hate exams",
+    phrase_de : "Ich hasse Tests"
+  }, 
+  {
+    phrase_en : "German is not a language you can learn",
+    phrase_de : "Deutsch ist keine Sprache, die du lernen kannst"
+  },
+  {
+    phrase_en : "Hello",
+    phrase_de : "Hallo"
+  }
+];
